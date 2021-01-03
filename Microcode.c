36cf44ab279906fc2752123e863c38ecf0c5dff7
Axel <axel.olsson@gmail.com>
2021-01-03 20:51:06 +0100

    a -= mem[immediate] (#13)

--- Microcode.c
+++ Microcode.c
@@ -6,12 +6,20 @@
 
 // 16 available control signals.
 enum Signal {
-    // 1 << 0 BUS OUT Address[0]
-    // 1 << 1 BUS OUT Address[1]
-    // 1 << 2 BUS OUT Address[2]
-    // 1 << 3 BUS IN Address[0]
-    // 1 << 4 BUS IN Address[1]
-    // 1 << 5 BUS IN Address[2]
+    ZERO_BUS_OUT = 0,
+    RAM_BUS_OUT = 1,
+    PC_BUS_OUT = 2,
+    IR_BUS_OUT = 3,
+    REGA_BUS_OUT = 4,
+    REGB_BUS_OUT = 5,
+    ALU_BUS_OUT = 6, // 0..2 bits bus out address.
+    RAM_BUS_IN = 1 << 3,
+    MAR_BUS_IN = 2 << 3,
+    PC_BUS_IN = 3 << 3,
+    IR_BUS_IN = 4 << 3,
+    REGA_BUS_IN = 5 << 3,
+    REGB_BUS_IN = 6 << 3,
+    OUT_BUS_IN = 7 << 3, // 3..5 bits bus in address.
     HALT = 1 << 6,
     PC_COUNT = 1 << 7,
     ALU_SUBTRACT = 1 << 8,
@@ -24,55 +32,94 @@ enum Signal {
     LAST_STEP = 1 << 15 // Last step bit.
 };
 
-enum BusOutAddress {
-    ZERO_OUT = 0,
-    RAM_OUT = 1,
-    PC_OUT = 2,
-    IR_OUT = 3,
-    REGA_OUT = 4,
-    REGB_OUT = 5,
-    ALU_OUT = 6
-};
-
-enum BusInAddress {
-    RAM_IN = 1 << 3,
-    MAR_IN = 2 << 3,
-    PC_IN = 3 << 3,
-    IR_IN = 4 << 3,
-    REGA_IN = 5 << 3,
-    REGB_IN = 6 << 3,
-    OUT_IN = 7 << 3
-};
-
 enum {
     MAX_NB_OF_CONTROL_STEPS = 16, // Low 4 bits of microcode address.
-    NB_OF_INSTRUCTIONS = 4,       // High 4 bits of microcode address.
+    NB_OF_INSTRUCTIONS = 16,      // High 4 bits of microcode address.
     DEFAULT_MICROCODE = HALT | LAST_STEP
 };
 
-uint16_t microcode[NB_OF_INSTRUCTIONS][MAX_NB_OF_CONTROL_STEPS] = {
-    // nop
+const uint16_t fetchSteps[] =
+    {PC_BUS_OUT | MAR_BUS_IN,
+     RAM_BUS_OUT | IR_BUS_IN | PC_COUNT};
+
+enum { FETCH_STEPS_LENGTH = sizeof(fetchSteps) / sizeof(fetchSteps[0]) };
+
+const uint16_t instructionSteps[NB_OF_INSTRUCTIONS][MAX_NB_OF_CONTROL_STEPS] = {
+    // 0x0: nop
     {LAST_STEP},
 
-    // mov a, [immediate address]
+    // 0x1: a = mem[immediate address]
     {
-        IR_OUT | MAR_IN,
-        RAM_OUT | REGA_IN | LAST_STEP,
+        IR_BUS_OUT | MAR_BUS_IN,
+        RAM_BUS_OUT | REGA_BUS_IN | LAST_STEP,
     },
 
-    // add a, [immediate address]
+    // 0x2: a += mem[immediate address]
     {
-        IR_OUT | MAR_IN,
-        RAM_OUT | REGB_IN,
-        ALU_OUT | REGA_IN | LAST_STEP,
+        IR_BUS_OUT | MAR_BUS_IN,
+        RAM_BUS_OUT | REGB_BUS_IN,
+        ALU_BUS_OUT | REGA_BUS_IN | LAST_STEP,
     },
 
-    // out a, immediate flags
+    // 0x3: a -= mem[immediate address]
     {
-        IR_OUT | OUT_SIGNED_IN,
-        REGA_OUT | OUT_IN | LAST_STEP,
+        IR_BUS_OUT | MAR_BUS_IN,
+        RAM_BUS_OUT | REGB_BUS_IN,
+        ALU_SUBTRACT | ALU_BUS_OUT | REGA_BUS_IN | LAST_STEP,
     },
+
+    // 0x4:
+    {LAST_STEP},
+
+    // 0x5:
+    {LAST_STEP},
+
+    // 0x6:
+    {LAST_STEP},
+
+    // 0x7:
+    {LAST_STEP},
+
+    // 0x8:
+    {LAST_STEP},
+
+    // 0x9:
+    {LAST_STEP},
+
+    // 0xa:
+    {LAST_STEP},
+
+    // 0xb:
+    {LAST_STEP},
+
+    // 0xc:
+    {LAST_STEP},
+
+    // 0xd:
+    {LAST_STEP},
+
+    // 0xe: out = a
+    {
+        REGA_BUS_OUT | OUT_BUS_IN | LAST_STEP,
+    },
+
+    // 0fx: halt
+    {HALT | LAST_STEP},
 };
+
+// Reset steps at 0x400 beacuse address bit 10 is held high until first encountered LAST_STEP.
+// Assumes IR and STEP is zero at startup.
+// TODO: Add reset stes at all possible IR values to remove 0 at start up requirement.
+const uint16_t resetAddress = 1 << 10;
+
+const uint16_t resetSteps[] =
+    {
+        ZERO_BUS_OUT | PC_BUS_IN,
+        REGA_BUS_IN,
+        REGB_BUS_IN,
+        OUT_BUS_IN,
+        MAR_BUS_IN,
+        RAM_BUS_OUT | IR_BUS_IN | LAST_STEP};
 
 enum OUTPUT_FORMAT {
     BINARY_OUTPUT,
@@ -88,12 +135,12 @@ int writeToFile(const enum OUTPUT_FORMAT format, const char *filename, const uin
         if (format == BINARY_OUTPUT) {
             fwrite(rom, sizeof(uint8_t), 2048, file);
         } else if (format == HEX_STRING_OUTPUT) {
-            enum { HEX_STRING_LEN = 5 }; // "0xFF\n" is 5 chars for each uint8_t.
+            enum { HEX_STRING_LEN = 3 }; // "FF\n" is 3 chars for each uint8_t.
 
             char hex[2048 * HEX_STRING_LEN];
 
             for (int i = 0; i < 2048; ++i) {
-                snprintf(hex + (i * HEX_STRING_LEN), HEX_STRING_LEN, "0x%02x", (*rom)[i]);
+                snprintf(hex + (i * HEX_STRING_LEN), HEX_STRING_LEN, "%02x", (*rom)[i]);
 
                 // Replace '\0' introduced by snprintf with '\n'.
                 hex[(i * HEX_STRING_LEN) + (HEX_STRING_LEN - 1)] = '\n';
@@ -110,12 +157,6 @@ int writeToFile(const enum OUTPUT_FORMAT format, const char *filename, const uin
 }
 
 void makeMicrocodes(uint8_t (*microcodeLow)[2048], uint8_t (*microcodeHigh)[2048]) {
-    const int fetchSteps[] =
-        {PC_OUT | MAR_IN,
-         RAM_OUT | IR_IN | PC_COUNT};
-
-    enum { FETCH_STEPS_LENGTH = sizeof(fetchSteps) / sizeof(fetchSteps[0]) };
-
     for (int i = 0; i < NB_OF_INSTRUCTIONS; ++i) {
         for (int s = 0; s < FETCH_STEPS_LENGTH; ++s) {
             const uint16_t address = (i << 4) + s;
@@ -127,7 +168,7 @@ void makeMicrocodes(uint8_t (*microcodeLow)[2048], uint8_t (*microcodeHigh)[2048
 
         for (int s = 0; s < (MAX_NB_OF_CONTROL_STEPS - FETCH_STEPS_LENGTH); ++s) {
             const uint16_t address = (i << 4) + FETCH_STEPS_LENGTH + s;
-            uint16_t code = microcode[i][s];
+            uint16_t code = instructionSteps[i][s];
 
             if (code == 0) {
                 code = DEFAULT_MICROCODE;
@@ -156,19 +197,6 @@ void makeMicrocodes(uint8_t (*microcodeLow)[2048], uint8_t (*microcodeHigh)[2048
             (*microcodeHigh)[address] = (code >> 8) & 0xff;
         }
     }
-
-    // Reset steps at 0x400 beacuse address bit 10 is held high until first encountered LAST_STEP.
-    // Assumes IR and STEP is zero at startup.
-    const uint16_t resetAddress = 1 << 10;
-
-    const int resetSteps[] =
-        {
-            ZERO_OUT | PC_IN,
-            REGA_IN,
-            REGB_IN,
-            OUT_IN,
-            MAR_IN,
-            RAM_OUT | IR_IN | LAST_STEP};
 
     for (int s = 0; s < (int)(sizeof(resetSteps) / sizeof(resetSteps[0])); ++s) {
         const uint16_t address = resetAddress + s;
